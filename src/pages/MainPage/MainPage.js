8dc39bc52a019db5137d1b09cee28efa0ca8b82c
dellyu03 <seunghwan081@gmail.com>
2024-03-22 23:48:04 +0900

    make main page

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,21 +1,25 @@
 import './MainPage.css'
 import SideBar from '../../components/SideBar/SideBar.js'
 import SelectCard from '../../components/SelectCard/SelectCard.js'
+import SearchBar from '../../components/SearchBar/SearchBar.js'
 
 export default function MainPage(){
     return(
         <body>
             <SideBar profileImg = "images/profileimg.png" userName = "Dellyu03" userIntr = "Developer Mode"/>
-            <div id = "SelectCard_container">
-                <SelectCard menuName = "Blue Sppyhere" imgSrc = "images/cocktail_img/blue spyhere.png" />
-                <SelectCard menuName = "Dekilla Sunrise" imgSrc = "images/cocktail_img/dekilla sunrise.jpg"/>
-                <SelectCard menuName = "Kahullua Milk" imgSrc = "images/cocktail_img/Rkffndk milk.png"/>
-                <SelectCard menuName = "Pinacolada" imgSrc = "images/cocktail_img/pinacolada.jpg"/>
-                <SelectCard menuName = "Faust" imgSrc = "images/cocktail_img/pahuse.jpg"/>
-                <SelectCard menuName = "Peach Crush" imgSrc = "images/cocktail_img/peach crush.png"/>
-                <SelectCard menuName = "Blue Hawai" imgSrc = "images/cocktail_img/blue hawai.jpg"/>
-                <SelectCard menuName = "Old Fashioned" imgSrc = "images/cocktail_img/old fashioned.jpg"/>
-            </div>
+            <content>
+                <SearchBar/>
+                <div id = "SelectCard_container">
+                    <SelectCard menuName = "Blue Sppyhere" imgSrc = "images/cocktail_img/blue spyhere.png" />
+                    <SelectCard menuName = "Dekilla Sunrise" imgSrc = "images/cocktail_img/dekilla sunrise.jpg"/>
+                    <SelectCard menuName = "Kahullua Milk" imgSrc = "images/cocktail_img/Rkffndk milk.png"/>
+                    <SelectCard menuName = "Pinacolada" imgSrc = "images/cocktail_img/pinacolada.jpg"/>
+                    <SelectCard menuName = "Faust" imgSrc = "images/cocktail_img/pahuse.jpg"/>
+                    <SelectCard menuName = "Peach Crush" imgSrc = "images/cocktail_img/peach crush.png"/>
+                    <SelectCard menuName = "Blue Hawai" imgSrc = "images/cocktail_img/blue hawai.jpg"/>
+                    <SelectCard menuName = "Old Fashioned" imgSrc = "images/cocktail_img/old fashioned.jpg"/>
+                </div>
+            </content>
         </body>
     );
 }
